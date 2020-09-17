4f91aadbbf398885834507dbee6b3f6001025b70
Manuel Santana <manuelarturosantana@gmail.com>
2020-09-17 12:30:21 -0600

    Put the loglog plot in derivative approximation

--- task_sheets/sheet2/derivative_approximation.py
+++ task_sheets/sheet2/derivative_approximation.py
@@ -1,4 +1,5 @@
 from math import cos
+import matplotlib.pyplot as plt
 
 def cosApprox(x, h):
     return (cos(x + h) - 2 * cos(x) + cos(x - h)) / (h ** 2)
@@ -9,9 +10,17 @@ count = 1
 while count <= 16:
     h.append(1 / 10 ** count)
     count += 1
-print("True Value-----------h---------Approximation---------Error Magnitude")
+print("h--Error Magnitude")
+difference = [];
 for a in h:
 	approx = cosApprox(2, a)
 	true = -cos(2)
 	diff = abs(true - approx)
-	print(f"True={true} h={a} approx={approx}, Error={diff}")
+	difference.append(diff)
+	print(f"h={a}, Error={diff}")
+
+plt.title("Centered Difference Approximation Error of cos Second Derivative")
+plt.xlabel("h values (logarithmic)")
+plt.ylabel("Absolute Error (logarithmic)")
+plt.loglog(h,difference)
+plt.show()
