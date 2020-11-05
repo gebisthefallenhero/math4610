3659d7436c7292d9e5077c458162d439f8e62ca1
Manuel Santana <manuelarturosantana@gmail.com>
2020-11-05 12:40:18 -0700

    Sheet6 task 3

--- lib/LotsOfRoots.py
+++ lib/LotsOfRoots.py
@@ -2,48 +2,54 @@ from NewtonsMethod import newtonsMethod
 from SecantMethod import secantMethod
 from FixedPoint import fixedPoint
 from Bisection import bisection
-from HybridMethod import hybridMethod
+from HybridMethod import hybridMethod, bisect
 from math import sin, exp, cos
 
-def hybridMethod(a,b,f,f_p,tol=.001,maxIter=1000):
+#This modified hybrid method recursively moves the left endpoint towards the orirgin.
+def modifiedHybridMethod(a,b,f,f_p,tol=.001,maxIter=1000,numIter=0, bestGuess =0):
+    if (b < 0):
+        return bestGuess
     if a > b:
         a,b = b,a
     if f(a)*f(b) > 0:
-        mid = (a + b) / 2
-        print('Error: [a b] does not contain a root')
+        if numIter < maxIter:
+            #Try the method again but moving b closer to the origin
+            return modifiedHybridMethod(a,b - .1,f,f_p,numIter=numIter + 1)
+        print('Error: No Root found in maximum iterations')
         return
     if f(a) == 0:
         return a
     if f(b) == 0:
         return b
+    if f(0) == 0:
+        return 0
     #Error B is the bisection error
     errorB = tol * 10
     iter = 0
-
     while errorB > tol and iter < maxIter:
         iter += 1
         mid = (a + b) * .5
         newtonsGuess = newtonsMethod(f,f_p,mid,tol=tol)
         errorN = abs(newtonsGuess - mid)
         if errorN < tol:
-            return newtonsGuess
+            #The case where the root actually zero is taken care of above
+            if bestGuess == 0:
+                bestGuess =  newtonsGuess
+                break
+            else:
+                if abs(newtonsGuess) < abs(bestGuess):
+                    bestGuess = newtonsGuess
         else:
             a,b = bisect(a,b,mid,f)
             if abs(b - a) < tol:
-                return a
+                if bestGuess == 0:
+                    bestGuess = a
+                else:
+                    if abs(a) < abs(bestGuess):
+                        bestGuess = a
+                break
+    return modifiedHybridMethod(a,b -.1,f,f_p,numIter=numIter + 1, bestGuess=bestGuess)
 
-
-def bisect(a,b,mid,f):
-    # The number of iterations needed to decrease by one order of magnitude using the bisection method.
-    ITER_ONE_ORDER_MAG = 4
-    for i in range(ITER_ONE_ORDER_MAG):
-        f_a = f(a)
-        f_mid = f(mid)
-        if f_mid * f_a < 0:
-            b = mid
-        else:
-            a = mid
-    return a,b
 
 
 if __name__ == '__main__':
@@ -62,4 +68,4 @@ if __name__ == '__main__':
     except:
         print("Newton's method failed")
 
-    print(f"The closest root to zero using the modified hybrid method is {modifiedHybridMethod(-5,6,function,functionP)}")
+    print(f"The closest root to zero using the modified hybrid method is {modifiedHybridMethod(-5,.547,function,functionP)}")
--- task_sheets/sheet6/Sheet6Solutions.md
+++ task_sheets/sheet6/Sheet6Solutions.md
@@ -20,4 +20,16 @@ One interesting thing while running this code what that a bad guess lead to a di
 I was duped. The newton's method crashed with both guesses. This is because the derivative at -5 and 6 is very close to zero, and therefore led to a division byzero error.
 
 ### Task 3
+The first step was to deal with the fact that the interval [-5,6] was not guaranteed to bracked a root by the intermedite value theorem. In order to deal with this the modified method added this block of code
+```
+if f(a)*f(b) > 0:
+    if numIter < maxIter:
+      #Try the method again but moving b closer to the origin
+       return modifiedHybridMethod(a,b - .1,f,f_p,numIter=numIter + 1)
+```
+What this code does is recursively try to find the root again this time moving b slightly closer to the origin. Then each time a root was found if that root was closer to zero than the last one it was saved as the best answer, and passed into the next recusive call. The recurssion ends if a negative b is passed into the code. This works since on that last iteration the best root between a and b at that point will be found, and compared with the current best root. The code output
 
+```
+The closest root to zero using the modified hybrid method is -0.48361071309741366
+```
+This compares is consistent with the answers in task 1 sincethe code has a root at + or - .4836 and therefor this is the best root.
